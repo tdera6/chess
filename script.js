77d694dacc8cbe1f452f1813892124b600b171ac
Tymon Dera <91739652+tdera06@users.noreply.github.com>
2023-04-04 23:57:46 +0200

    added board tiles style

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 const tiles = document.querySelectorAll(".tile");
 
+let i = 0;
 tiles.forEach((e) => {
+  e.innerHTML = i;
+  i++;
   e.addEventListener("click", () => {
     console.log(e.dataset.column, e.dataset.row);
   });
