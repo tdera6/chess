6cdc9ef5d90d9b9821f800ee20315730d9f2a4ac
Tymon Dera <91739652+tdera06@users.noreply.github.com>
2023-04-05 21:15:13 +0200

    js pieces coordinates

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
 
 const tiles = document.querySelectorAll(".tile");
 
-let i = 0;
-tiles.forEach((e) => {
-  e.innerHTML = i;
-  i++;
-  e.addEventListener("click", () => {
-    console.log(e.dataset.column, e.dataset.row);
+const gameState = [
+  ["r", "n", "b", "q", "k", "b", "n", "r"],
+  ["p", "p", "p", "p", "p", "p", "p", "p"],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["P", "P", "P", "P", "P", "P", "P", "P"],
+  ["R", "N", "B", "Q", "K", "B", "N", "R"],
+];
+
+gameState.forEach((e) => {
+  e.forEach((x) => {
+    if (x === "r") {
+    }
   });
 });
+
+tiles.forEach((e) => {});
