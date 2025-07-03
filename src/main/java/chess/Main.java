9c74612c3887b77e70dd83d962311ac271acb9be
Tymon Dera <dera.inf@wp.pl>
2025-07-03 22:55:22 +0200

    board display

---
+++ src/main/java/chess/Main.java
@@ -0,0 +1,10 @@
+package chess;
+
+import chess.logic.Board;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.printToConsole();
+    }
+}
