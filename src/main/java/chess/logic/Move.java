6f2b831677b9594a93ae769e298e9b0c10bb2a1d
Tymon Dera <dera.inf@wp.pl>
2025-07-05 23:56:20 +0200

    new fundamental classes

---
+++ src/main/java/chess/logic/Move.java
@@ -0,0 +1,10 @@
+package chess.logic;
+
+public class Move {
+    private final Square from, to;
+
+    public Move(Square from, Square to) {
+        this.from = from;
+        this.to = to;
+    }
+}
---
+++ src/main/java/chess/logic/Square.java
@@ -0,0 +1,18 @@
+package chess.logic;
+
+public class Square {
+    private final int row, column;
+
+    public Square(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
