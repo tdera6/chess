4f50efc88a497ac1c6eb2a7d196d42473f00548c
Tymon Dera <dera.inf@wp.pl>
2025-07-06 23:11:39 +0200

    added Rook legal moves

--- src/main/java/chess/logic/pieces/Rook.java
+++ src/main/java/chess/logic/pieces/Rook.java
@@ -5,6 +5,7 @@ import chess.logic.Color;
 import chess.logic.Move;
 import chess.logic.Square;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -23,6 +24,24 @@ public class Rook extends Piece {
 
     @Override
     public List<Move> getLegalMoves(Board board, Square currentSquare) {
-        return null;
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        int originalRow = currentSquare.getRow();
+        int originalColumn = currentSquare.getColumn();
+
+        List<Move> legalMoves = new ArrayList<>();
+
+        for (int[] direction : directions) {
+            int row = originalRow + direction[0];
+            int column = originalColumn + direction[1];
+
+            while (board.isValidSquare(row, column)) {
+                legalMoves.add(new Move(currentSquare, new Square(row, column)));
+                row += direction[0];
+                column += direction[1];
+            }
+        }
+
+        return legalMoves;
     }
 }
