708af49a0c55aa86219e1c12763bab80d80f33cc
Tymon Dera <dera.inf@wp.pl>
2025-07-11 11:09:09 +0200

    removed unused imports from sliding pieces (Bishop, Queen, Rook)

--- src/main/java/chess/logic/pieces/Rook.java
+++ src/main/java/chess/logic/pieces/Rook.java
@@ -1,12 +1,6 @@
 package chess.logic.pieces;
 
-import chess.logic.Board;
 import chess.logic.Color;
-import chess.logic.Move;
-import chess.logic.Square;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Rook extends SlidingPiece {
     public Rook(Color color) {
