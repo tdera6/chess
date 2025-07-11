54ce7674e24c793e801e937bd3514be2259e55f8
Tymon Dera <dera.inf@wp.pl>
2025-07-11 23:25:54 +0200

    changed test position

--- src/main/java/chess/logic/Board.java
+++ src/main/java/chess/logic/Board.java
@@ -10,7 +10,7 @@ public class Board {
     private final Piece[][] board = new Piece[8][8];
 
     public Board() {
-        loadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        loadFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
     }
 
     private void setupDefaultPieces() {
