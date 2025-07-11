2c0e505dd094ac641b0ea10c93698f44643aca76
Tymon Dera <dera.inf@wp.pl>
2025-07-11 19:54:40 +0200

    enhanced move validation in Game with input parsing and legality checks, added makeMove method for executing moves

--- src/main/java/chess/logic/Game.java
+++ src/main/java/chess/logic/Game.java
@@ -1,5 +1,8 @@
 package chess.logic;
 
+import chess.logic.pieces.Piece;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
@@ -19,24 +22,64 @@ public class Game {
         while (!gameOver) {
             System.out.println("Current player: " + currentPlayer);
             board.printBoard();
-            board.printLegalMoves(currentPlayer);
             String moveAsString = scanner.next();
+            Square[] squares = {};
+            try {
+                squares = ParseMove(moveAsString);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("Incorrect move. Try again");
+                continue;
+            }
+
+            List<Move> allLegalMoves = board.getAllLegalMoves(currentPlayer);
+
+            Move move = new Move(squares[0], squares[1]);
+
+            System.out.println(move);
+            System.out.println("=============");
+            for (Move legalMove : allLegalMoves) {
+                System.out.println(legalMove);
+            }
+
+            if (allLegalMoves.contains(move)) {
+                makeMove(move);
+            } else {
+                System.out.println("Error: This move is not legal");
+                System.out.println("Try again");
+                continue;
+            }
             currentPlayer = currentPlayer == Color.WHITE ? Color.BLACK : Color.WHITE;
         }
     }
 
     private Square[] ParseMove(String move) {
-        int firstRow = 8 - Integer.parseInt(move.substring(1));
+
+        if (move.length() != 4) throw new IllegalArgumentException("Incorrect input length");
+
+        int firstRow = 8 - Integer.parseInt(move.substring(1, 2));
         int firstColumn = move.charAt(0) - 'a';
 
-        int secondRow = 8 - Integer.parseInt(move.substring(3));
+        int secondRow = 8 - Integer.parseInt(move.substring(3, 4));
         int secondColumn = move.charAt(2) - 'a';
+
+        if (firstRow < 0 || firstRow > 7 || firstColumn < 0 || firstColumn > 7 || secondRow < 0 || secondRow > 7 || secondColumn < 0 || secondColumn > 7) {
+            throw new IllegalArgumentException("There is no such square");
+        }
 
         Square firstSquare = new Square(firstRow, firstColumn);
         Square secondSquare = new Square(secondRow, secondColumn);
 
-        Square[] squares = {firstSquare, secondSquare};
+        return new Square[]{firstSquare, secondSquare};
+    }
 
-        return squares;
+    public void makeMove(Move move) {
+        Square startingSquare = move.getFrom();
+        Square endingSquare = move.getTo();
+
+        Piece piece = board.getPiece(startingSquare.getRow(), startingSquare.getColumn());
+        board.setPiece(startingSquare.getRow(), startingSquare.getColumn(), null);
+        board.setPiece(endingSquare.getRow(), endingSquare.getColumn(), piece);
+
     }
 }
