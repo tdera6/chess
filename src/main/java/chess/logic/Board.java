c39156fdaf36fce8485ab393675de1792bba4d7b
Tymon Dera <dera.inf@wp.pl>
2025-07-11 23:10:22 +0200

    added FEN loading to Board for initializing positions from FEN strings

--- src/main/java/chess/logic/Board.java
+++ src/main/java/chess/logic/Board.java
@@ -10,7 +10,7 @@ public class Board {
     private final Piece[][] board = new Piece[8][8];
 
     public Board() {
-        setupDefaultPieces();
+        loadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     }
 
     private void setupDefaultPieces() {
@@ -74,6 +74,47 @@ public class Board {
         }
 
         return allLegalMoves;
+    }
+
+    public void loadFEN(String fen) {
+        String[] fenRows = fen.split("/");
+        String[] fenInformation = fenRows[7].split(" ");
+        fenRows[7] = fenInformation[0];
+
+        for (int i = 0; i < 8; i++) {
+            String row = fenRows[i];
+            int column = 0;
+
+            for (char c : row.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    column += Character.getNumericValue(c);
+                } else {
+                    setPiece(i, column++, loadPiece(c));
+                }
+            }
+        }
+    }
+
+    public void loadFEN() {
+        setupDefaultPieces();
+    }
+
+    private Piece loadPiece(char c) {
+        return switch (c) {
+            case 'P' -> new Pawn(Color.WHITE);
+            case 'p' -> new Pawn(Color.BLACK);
+            case 'R' -> new Rook(Color.WHITE);
+            case 'r' -> new Rook(Color.BLACK);
+            case 'N' -> new Knight(Color.WHITE);
+            case 'n' -> new Knight(Color.BLACK);
+            case 'B' -> new Bishop(Color.WHITE);
+            case 'b' -> new Bishop(Color.BLACK);
+            case 'Q' -> new Queen(Color.WHITE);
+            case 'q' -> new Queen(Color.BLACK);
+            case 'K' -> new King(Color.WHITE);
+            case 'k' -> new King(Color.BLACK);
+            default -> null;
+        };
     }
 
     public boolean isValidSquare(int row, int col) {
